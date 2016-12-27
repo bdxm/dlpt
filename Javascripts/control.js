dc3ea0f68b2112262dd51ba878af128b39da4d7d
xiangzhelo <949460716@qq.com>
2016-12-27 11:09:31 +0800

    版本控制

--- Javascripts/control.js
+++ Javascripts/control.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
     //当前js版本
-    var Version = '3.6';
+    var Version = '3.7';
     function getmoney() {
         $.get("Apps?module=Agent&action=getbalance",function(data){
             data=$.parseJSON(data);
